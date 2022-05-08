60f84429fed79848c16df0941eedf3ff29ea0ee1
BrycePy <su287@bath.ac.uk>
2022-05-08 03:15:57 +0100

    add arc text gen

--- SignGen.py
+++ SignGen.py
@@ -1,6 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
+import io
+from wand.image import Image as ImageWand
+from wand.font import Font
+from wand.display import display
+
 def find_coeffs(pa, pb):
     matrix = []
     for p1, p2 in zip(pa, pb):
@@ -30,7 +35,37 @@ def glitch_sign(text, font_size):
     glitch.paste(img, (0, 0), img)
     return glitch
 
+def glitch_rainbow(text, font_size=60):
+    glitch = Image.open("glitchrainbow.png")
+
+    with io.BytesIO() as f:
+        font = ImageFont.truetype("COMIC.TTF", font_size)
+        img1 = Image.new('RGBA', (500, 200), (255, 255, 255, 0))
+        draw = ImageDraw.Draw(img1)
+        draw.text((250, 85), text, fill=(0, 0, 0), anchor="mm", font=font)
+        img1.save(f, "PNG")
+        image_blob = f.getvalue()
+
+    with ImageWand(blob = image_blob) as img:
+        img.background_color = 'transparent'
+        img.font = Font('Arial', 20)
+        img.virtual_pixel = 'transparent'
+        img.distort('arc', (160, 0))
+        img.format = 'png'
+        f = io.BytesIO()
+        img.save(file=f)
+        f.seek(0)
+        image_warp = Image.open(f)
+
+    glitch.paste(image_warp, (-15, 0), image_warp)
+    f.close()
+    return glitch
+
 if __name__ == "__main__":
     img = glitch_sign("Hello, world!", 40)
     img.save("output.png")
+    img.show()
+
+    img = glitch_rainbow("Hello, world!")
+    img.save("output_rainbow.png")
     img.show()
